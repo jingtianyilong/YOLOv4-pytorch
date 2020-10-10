1cfde55636c533a955aef4db221c355e796c745e
ZIJIGUO <zijie.guo@daimler.com>
2020-10-10 10:10:23 +0800

    missed val process in train()

--- utils/cocodataset.py
+++ utils/cocodataset.py
@@ -28,12 +28,10 @@ class COCODataset(Dataset):
         """
         self.model_type = model_type
         
-        self.coco = COCO()
         self.val_dataset = data.Build_VAL_Dataset(cfg)
         self.val_loader = DataLoader(val_dataset, batch_size=cfg.VAL.BATCH_SIZE, shuffle=True, num_workers=8,
                             pin_memory=True, drop_last=True, collate_fn=val_collate)
-        self.coco = convert_to_coco_api(bbox_fmt='coco')
-        self.coco.createIndex()
+        self.coco = convert_to_coco_api(bbox_fmt='yolo')
         
         self.ids = self.coco.getImgIds()
         if debug:
@@ -44,16 +42,16 @@ class COCODataset(Dataset):
         self.max_labels = 50
         self.img_size = cfg.VAL.TEST_IMG_SIZE
         self.min_size = 1
-        f = open(lable_path, 'r', encoding='utf-8')
-        for line in f.readlines():
-            data = line.rstrip().split(" ")
-            truth[data[0]] = []
-            if len(data) > 1:
-                for i in data[1:]:
-                    truth[data[0]].append([int(float(j)) for j in i.split(',')])
+        # f = open(lable_path, 'r', encoding='utf-8')
+        # for line in f.readlines():
+        #     data = line.rstrip().split(" ")
+        #     truth[data[0]] = []
+        #     if len(data) > 1:
+        #         for i in data[1:]:
+        #             truth[data[0]].append([int(float(j)) for j in i.split(',')])
 
-        self.truth = truth
-        self.imgs = list(self.truth.keys())
+        # self.truth = truth
+        # self.imgs = list(self.truth.keys())
 
     def __len__(self):
         return len(self.ids)
@@ -78,31 +76,42 @@ class COCODataset(Dataset):
                 dx, dy (int): pad size
             id_ (int): same as the input index. Used for evaluation.
         """
-        # id_ = self.ids[index]
-        id_ = int(os.path.basename(img_path).split(".")[0])
-        # anno_ids = self.coco.getAnnIds(imgIds=[int(id_)], iscrowd=None)
-        # annotations = self.coco.loadAnns(anno_ids)
+        # id_ = int(os.path.basename(img_path).split(".")[0])
+        # img_path = self.imgs[index]
+        # bboxes_with_cls_id = np.array(self.truth.get(img_path), dtype=np.float)
+        # boxes = bboxes_with_cls_id[...,:4]
+        # boxes[..., 2:] = boxes[..., 2:] - boxes[..., :2]  # box width, box height
+        # img = cv2.imread(os.path.join(self.cfg.dataset_dir, img_path))
+        # img, info_img = preprocess(img, self.img_size, jitter=self.jitter,
+        #                            random_placing=self.random_placing)
 
-        # lrflip = False
-        # if np.random.rand() > 0.5 and self.lrflip == True:
-        #     lrflip = True
+        # img = np.transpose(img / 255., (2, 0, 1))
+
+        # labels = []
+        # for box in boxes:
+        #     if box[2] > self.min_size and box[3] > self.min_size:
+        #         labels.append(box)
+
+        # padded_labels = np.zeros((self.max_labels, 5))
+        # if len(labels) > 0:
+        #     labels = np.stack(labels)
+        #     labels = label2yolobox(labels, info_img, self.img_size, lrflip)
+        #     padded_labels[range(len(labels))[:self.max_labels]
+        #                   ] = labels[:self.max_labels]
+        # padded_labels = torch.from_numpy(padded_labels)
+                id_ = self.ids[index]
+
+        anno_ids = self.coco.getAnnIds(imgIds=[int(id_)], iscrowd=None)
+        annotations = self.coco.loadAnns(anno_ids)
+
+        lrflip = False
+        if np.random.rand() > 0.5 and self.lrflip == True:
+            lrflip = True
 
         # load image and preprocess
-        # img_file = os.path.join(self.data_dir, self.name,
-        #                         '{:012}'.format(id_) + '.jpg')
-        img_path = self.imgs[index]
-        bboxes_with_cls_id = np.array(self.truth.get(img_path), dtype=np.float)
-        boxes = bboxes_with_cls_id[...,:4]
-        boxes[..., 2:] = boxes[..., 2:] - boxes[..., :2]  # box width, box height
-        img = cv2.imread(os.path.join(self.cfg.dataset_dir, img_path))
-
-        # img = cv2.imread(img_file)
-
-        # if self.json_file == 'instances_val5k.json' and img is None:
-        #     img_file = os.path.join(self.data_dir, 'train2017',
-        #                             '{:012}'.format(id_) + '.jpg')
-        #     img = cv2.imread(img_file)
-        # assert img is not None
+        img_file = os.path.join(self.data_dir, self.name,
+                                '{:012}'.format(id_) + '.jpg')
+        img = cv2.imread(img_file)
 
         img, info_img = preprocess(img, self.img_size, jitter=self.jitter,
                                    random_placing=self.random_placing)
@@ -110,26 +119,27 @@ class COCODataset(Dataset):
 
         img = np.transpose(img / 255., (2, 0, 1))
 
-        # if lrflip:
-        #     img = np.flip(img, axis=2).copy()
 
         # load labels
         labels = []
-        for box in boxes:
-            if box[2] > self.min_size and box[3] > self.min_size:
-                labels.append(box)
+        for anno in annotations:
+            if anno['bbox'][2] > self.min_size and anno['bbox'][3] > self.min_size:
+                labels.append([])
+                labels[-1].append(self.class_ids.index(anno['category_id']))
+                labels[-1].extend(anno['bbox'])
 
         padded_labels = np.zeros((self.max_labels, 5))
         if len(labels) > 0:
             labels = np.stack(labels)
-            labels = label2yolobox(labels, info_img, self.img_size, lrflip)
+            if 'YOLO' in self.model_type:
+                labels = label2yolobox(labels, info_img, self.img_size, lrflip)
             padded_labels[range(len(labels))[:self.max_labels]
                           ] = labels[:self.max_labels]
         padded_labels = torch.from_numpy(padded_labels)
 
         return img, padded_labels, info_img, id_
 
-    def convert_to_coco_api(self, bbox_fmt='voc'):
+    def convert_to_coco_api(self):
         """
         """
         print("in function convert_to_coco_api...")
@@ -149,15 +159,6 @@ class COCODataset(Dataset):
             img_dict['width'] = img.shape[-1]
             dataset['images'].append(img_dict)
             bboxes = targets["boxes"]
-            # to coco format: xmin, ymin, w, h
-            if bbox_fmt.lower() == "voc":  # xmin, ymin, xmax, ymax
-                bboxes[:, 2:] -= bboxes[:, :2]
-            elif bbox_fmt.lower() == "yolo":  # xcen, ycen, w, h
-                bboxes[:, :2] = bboxes[:, :2] - bboxes[:, 2:]/2
-            elif bbox_fmt.lower() == "coco":
-                pass
-            else:
-                raise ValueError(f"bounding box format {bbox_fmt} not supported!")
             
             bboxes = bboxes.tolist()
             labels = targets['labels'].tolist()
